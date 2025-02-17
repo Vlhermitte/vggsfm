371293ace1aa5e0f10fa0542d3534518a26e5e8e
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-02-17 19:12:51 +0100

    Changing max_tri_points_num param for 16 GB GPU

--- vggsfm/utils/triangulation.py
+++ vggsfm/utils/triangulation.py
@@ -683,7 +683,8 @@ def triangulate_tracks(
     min_tri_angle=1.5,
     track_vis=None,
     track_score=None,
-    max_tri_points_num=819200,
+    # max_tri_points_num=819200, # FOR 32GB GPU
+    max_tri_points_num=409600,  # FOR 16GB GPU
 ):
     """
     Triangulate Tracks. If necessary, process tracks in smaller chunks to avoid memory issues during triangulation.
